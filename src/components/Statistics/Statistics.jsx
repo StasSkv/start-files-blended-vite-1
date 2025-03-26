cfe6f61109e9ba3fbd28c89811b16075e47117c9
StasSkv <stas000123@gmail.com>
2025-03-26 22:12:04 +0200

    a

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,34 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import  StatisticsItem  from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
+import StatisticsItem from '../StatisticsItem/StatisticsItem';
+import css from './Statistics.module.css';
+const icons = [
+  <FaRegThumbsUp key="1" />,
+  <MdPeople key="2" />,
+  <MdOutlineProductionQuantityLimits key="3" />,
+  <GiTreeDoor key="4" />,
+];
 
-const Statistics = () => {
-  return <h2>Statistics</h2>;
+const Statistics = ({ title, stats }) => {
+  return (
+    <div>
+      <h3 className={css.title}>{title}</h3>
+      <ul className={css.list}>
+        {stats.map((stat, index) => {
+          return (
+            <li key={Number(stat.id)} className={css.item}>
+              <StatisticsItem
+                title={stat.title}
+                total={stat.total}
+                icons={icons[index]}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Statistics;
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,16 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import css from './StatisticsItem.module.css';
+import { IconContext } from 'react-icons';
+
+const StatisticsItem = ({ title, total, icons }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ color: 'black', size: '30px' }}>
+        {icons}
+      </IconContext.Provider>
+      <span className={css.counter}>{total}</span>
+      <p className={css.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
